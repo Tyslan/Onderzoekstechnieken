c4cee24a7300effb657f4d0617eddc14925f1d32
Tyslan <verschingellorenz@hotmail.com>
2015-04-15 11:41:09 +0200

    Simulatie mischance array

--- Simulator/src/com/philihp/bj/Simulator.java
+++ Simulator/src/com/philihp/bj/Simulator.java
@@ -19,21 +19,21 @@ import java.util.List;
  * @author Lorenz
  */
 public class Simulator {
+
     /**
      * Number of simulations that are runned
-    */
+     */
     public static int NUMBER_OF_SIMULATIONS = 100000;
-    
+
     /**
      * Number of hands that are played in a simulation
-    */
+     */
     public static int NUMBER_OF_HANDS = 100;
-    
+
     /**
      * Chance player chooses wrong response
-    */
-    public static double CHANCE_OF_WRONG_RESPONSE = 0.0;
-
+     */
+    public static double CHANCE_OF_WRONG_RESPONSE = 0.075;
 
     /**
      * Number of decks in a shoe
@@ -72,58 +72,62 @@ public class Simulator {
 
     public static void main(String[] args) {
         long startTime = System.nanoTime();
-        
+
         randomizer = new SecureRandom();
-        Player player = new ThorpeBasicPlayerWithMisChance(CHANCE_OF_WRONG_RESPONSE);
-        Player dealer = new DealerPlayer();
 
-        long money = 0;
-        long handsPlayed = 0;
-        long simulationsRunned = 0;
-        
-        List<SimulationRunResult> resultList = new ArrayList<>();
-        
-        while (simulationsRunned < NUMBER_OF_SIMULATIONS) {
-            Deck deck = new Deck(SHOE_SIZE, player);
-            deck.shuffle(randomizer);
-            player.resetCount(SHOE_SIZE);
-            
-             try {
-                player.setNumberOfMistakes(0);
-            } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
-            }
-            
-            simulationsRunned++;
-            while (handsPlayed < NUMBER_OF_HANDS) {
-                handsPlayed++;
-                if ((float) deck.size() / deck.getInitialSize() > CUT_CARD_PENETRATION) {
-                    deck = new Deck(SHOE_SIZE, player);
-                    deck.shuffle(randomizer);
-                    player.resetCount(SHOE_SIZE);
+        double[] mischances = {0.0, 0.01, 0.02, 0.03, 0.05, 0.075, 0.1, 0.125, 0.15, 0.175, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0};
+
+        for (int i = 0; i < mischances.length; i++) {
+            Player player = new ThorpeBasicPlayerWithMisChance(mischances[i]);
+            Player dealer = new DealerPlayer();
+
+            long money = 0;
+            long handsPlayed = 0;
+            long simulationsRunned = 0;
+
+            List<SimulationRunResult> resultList = new ArrayList<>();
+
+            while (simulationsRunned < NUMBER_OF_SIMULATIONS) {
+                Deck deck = new Deck(SHOE_SIZE, player);
+                deck.shuffle(randomizer);
+                player.resetCount(SHOE_SIZE);
+
+                try {
+                    player.setNumberOfMistakes(0);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
                 }
 
-                List<Hand> playerHands = new ArrayList<>(1);
-                playerHands.add(new Hand(player.bet(), deck.draw(), deck.draw(), false));
-                Hand dealerHand = new Hand(deck.draw(), deck.draw());
-                money -= playerHands.get(0).getBet();
+                simulationsRunned++;
+                while (handsPlayed < NUMBER_OF_HANDS) {
+                    handsPlayed++;
+                    if ((float) deck.size() / deck.getInitialSize() > CUT_CARD_PENETRATION) {
+                        deck = new Deck(SHOE_SIZE, player);
+                        deck.shuffle(randomizer);
+                        player.resetCount(SHOE_SIZE);
+                    }
 
-                if (dealerHand.getValue() == 21) {
+                    List<Hand> playerHands = new ArrayList<>(1);
+                    playerHands.add(new Hand(player.bet(), deck.draw(), deck.draw(), false));
+                    Hand dealerHand = new Hand(deck.draw(), deck.draw());
+                    money -= playerHands.get(0).getBet();
+
+                    if (dealerHand.getValue() == 21) {
                     //dealer has blackjack. do not play out hands, just leave money on
-                    //table and start over.
-                    continue;
+                        //table and start over.
+                        continue;
+                    }
+
+                    money += playoutPlayer(player, deck, playerHands, dealerHand);
+                    playoutDealer(deck, dealer, dealerHand);
+
+                    for (Hand playerHand : playerHands) {
+                        money += payout(playerHand, dealerHand);
+                    }
                 }
 
-                money += playoutPlayer(player, deck, playerHands, dealerHand);
-                playoutDealer(deck, dealer, dealerHand);
+                double houseEdge = -(100 * (double) money / (handsPlayed * MIN_BET));
 
-                for (Hand playerHand : playerHands) {
-                    money += payout(playerHand, dealerHand);
-                }
-            }
-            
-            double houseEdge = (100 * (double) money / (handsPlayed * MIN_BET));
-            
 //            System.out.println("Simulation " + simulationsRunned + ":");
 //            System.out.println("Hands Played:    " + handsPlayed);
 //            System.out.println("Money:           " + money);
@@ -131,24 +135,24 @@ public class Simulator {
 //            System.out.println("House Edge :     " + houseEdge + "%");
 //            System.out.println("Mistakes:        " + player.getNumberOfMistakes());
 //            System.out.println();
-            
-            resultList.add(new SimulationRunResult(simulationsRunned, money, houseEdge, player.getNumberOfMistakes()));
-            
-            handsPlayed = 0;
-            money = 0;
+                resultList.add(new SimulationRunResult(simulationsRunned, money, houseEdge, player.getNumberOfMistakes()));
+
+                handsPlayed = 0;
+                money = 0;
+            }
+
+            double avgPayout = resultList.stream().mapToLong(SimulationRunResult::getPayout).average().getAsDouble();
+            double avgHouseEdge = resultList.stream().mapToDouble(SimulationRunResult::getHouseEdge).average().getAsDouble();
+            double avgMistakes = resultList.stream().mapToInt(SimulationRunResult::getNumberOfMistakes).average().getAsDouble();
+            System.out.println("Average payout: " + avgPayout);
+            System.out.println("Average House Edge: " + avgHouseEdge + "%");
+            System.out.println("Averages mistakes: " + avgMistakes);
+
+            System.out.println("Runtime: " + ((float) (System.nanoTime() - startTime) / 1000000000f) + " seconds");
+
+            String path = "F:\\result " + NUMBER_OF_SIMULATIONS + "simulations " + mischances[i] + "Mischance.csv";
+            writeCSV(path, resultList);
         }
-        
-        double avgPayout = resultList.stream().mapToLong(SimulationRunResult::getPayout).average().getAsDouble();
-        double avgHouseEdge = resultList.stream().mapToDouble(SimulationRunResult::getHouseEdge).average().getAsDouble();
-        double avgMistakes = resultList.stream().mapToInt(SimulationRunResult::getNumberOfMistakes).average().getAsDouble();
-        System.out.println("Average payout: " + avgPayout);
-        System.out.println("Average House Edge: " + avgHouseEdge +"%");
-        System.out.println("Averages mistakes: " + avgMistakes);
-        
-        System.out.println("Runtime: " + ((float) (System.nanoTime() - startTime) / 1000000000f) + " seconds");
-        
-        String path = "F:\\result "+ NUMBER_OF_SIMULATIONS+"simulations " + CHANCE_OF_WRONG_RESPONSE +"Mischance.csv";
-        writeCSV(path, resultList);
     }
 
     private static float playoutPlayer(Player player, Deck deck,
@@ -260,15 +264,14 @@ public class Simulator {
             throw new RuntimeException("Player: " + playerHand + ", Dealer: " + dealerHand);
         }
     }
-    
-    private static void writeCSV(String fileName, List<SimulationRunResult> list){
-        try
-	{
-	    FileWriter writer = new FileWriter(fileName);
- 
-	    writer.append("Simulation number");
-	    writer.append(';');
-	    writer.append("Hands Played");
+
+    private static void writeCSV(String fileName, List<SimulationRunResult> list) {
+        try {
+            FileWriter writer = new FileWriter(fileName);
+
+            writer.append("Simulation number");
+            writer.append(';');
+            writer.append("Hands Played");
             writer.append(';');
             writer.append("Minimum Bet");
             writer.append(';');
@@ -277,14 +280,14 @@ public class Simulator {
             writer.append("House Edge (%)");
             writer.append(';');
             writer.append("Number of Mistakes");
-	    writer.append('\n');
-            
-            for(SimulationRunResult result: list){
+            writer.append('\n');
+
+            for (SimulationRunResult result : list) {
                 writer.append(String.valueOf(result.getSimulationNr()));
                 writer.append(';');
                 writer.append(String.valueOf(NUMBER_OF_HANDS));
                 writer.append(';');
-                writer.append(String.valueOf(MIN_BET));              
+                writer.append(String.valueOf(MIN_BET));
                 writer.append(';');
                 writer.append(String.valueOf(result.getPayout()));
                 writer.append(';');
@@ -293,13 +296,11 @@ public class Simulator {
                 writer.append(String.valueOf(result.getNumberOfMistakes()));
                 writer.append('\n');
             }
- 
-	    writer.flush();
-	    writer.close();
-	}
-	catch(IOException e)
-	{
-	     e.printStackTrace();
-	} 
+
+            writer.flush();
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
